7d8cbb2f6531f555153dd4549dce9c2c72df33b0
Justin Brown <justinbrown@Justins-MacBook-Pro.local>
2018-05-18 00:06:00 -0700

    added redux form to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import PortfolioItems from './reducer_portfolio_items';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-	portfolio_items: PortfolioItems
+	portfolio_items: PortfolioItems,
+	form: reduxFormReducer
 });
 
 export default rootReducer;

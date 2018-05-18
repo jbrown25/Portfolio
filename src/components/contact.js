a7737e05d170e7f9fbe81f0694674628847e9d1c
Justin Brown <justinbrown@Justins-MacBook-Pro.local>
2018-05-18 00:04:38 -0700

    built and styled contact page, still have to wire up submit function

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
+import anime from 'animejs';
+import '../styles/contact.css';
+import ContactForm from './contact_form';
 
-const Contact = () => {
-	return (
-		<section className='contact'>
-			Contact
-		</section>
-	);
-};
 
-export default Contact;
+export default class Contact extends Component {
+
+	componentDidMount(){
+		const contact_timeline = anime.timeline();
+		contact_timeline
+		.add({
+			targets: this.address_container,
+			opacity: [0, 1],
+			translateY: [50, 0],
+			duration: 500,
+			delay: 250,
+			easing: 'easeOutQuad'		
+		})
+		.add({
+			targets: this.form_container,
+			opacity: [0, 1],
+			translateY: [50, 0],
+			duration: 500,
+			easing: 'easeOutQuad',
+			offset: '-=250'
+		});
+	}
+	
+	render(){
+		return (
+			<section className='contact'>
+				<div className='contact_container'>
+					<div className='address_container' ref={el => this.address_container = el}>
+						<h1 ref={el => this.thingy = el}>Get in touch.</h1>
+						<p>For inquiries regarding projects, jobs, or if you just need a hand with your homework.  Let me know how I can help!</p>
+						<address>
+							Justin.Brown345@gmail.com<br />
+							(509) 531-1368<br />
+							425 23rd Ave S Apt A205<br />
+							Seattle, WA 98144<br />
+						</address>
+					</div>
+					<div className='form_container' ref={el => this.form_container = el}>
+						<ContactForm />
+					</div>
+				</div>
+			</section>
+		);
+	}	
+}
+
